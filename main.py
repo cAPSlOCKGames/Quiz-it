7b940502957c469ddd152fe7377bf4bcd98e1d34
cAPSlOCKGames <83343855+cAPSlOCKGames@users.noreply.github.com>
2022-03-25 22:02:13 +0300

    ui progress
    finished the options buttons.

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ import sys
 import json
 import os
 
-data = {}
+
+
+pygame.init()
+pygame.font.init()
 
 if os.path.exists('questions.json'):
     with open('questions.json', 'r') as f:
         data = json.load(f)
+else:
+    pass
 
 num = 2
 currentdict = (list(data.values())[(num - 1)])
 
-print(currentdict["question"], currentdict[currentdict["correct"]])     #for debuging purposes
+data = {}
+currentanswer = ''
+font = pygame.font.SysFont('Comic Sans MS', 30)
+A_text = font.render(currentdict["opA"], True,(255, 255, 255))
+B_text = font.render(currentdict["opB"], True,(255, 255, 255))
+C_text = font.render(currentdict["opC"], True,(255, 255, 255))
+D_text = font.render(currentdict["opD"], True,(255, 255, 255))
 
-pygame.init()
+print(currentdict["question"], currentdict[currentdict["correct"]])     #for debuging purposes
 
 (width, height) = (1280, 720)
 window = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Quiz-it')
 
 class options:
     def __init__(self, width, height, X, Y, color, answer):
@@ -28,15 +40,41 @@ class options:
         self.color = color
         self.answer = answer
         self.button = pygame.Rect(self.X, self.Y, self.width, self.height)
+        self.pressed = False
+
     def draw(self):
         pygame.draw.rect(window, self.color, self.button)
 
+        status = False
+        mousepos = pygame.mouse.get_pos()
+
+        if self.button.collidepoint(mousepos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.pressed == False:
+                self.pressed = True
+                status = True
+
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.pressed = False
+            status = False
+
+        return status
+
+opA = options(500, 175, 125, (height - 60 - 175 - 175), (0, 0, 0), currentdict["opA"])
+opB = options(500, 175, (width - 500 - 125), (height - 60 - 175 - 175), (0, 0, 0), currentdict["opB"])
+opC = options(500, 175, 125, (height - 30 - 175), (0, 0, 0), currentdict["opC"])    #op is short for option
+opD = options(500, 175, (width - 500 - 125), (height - 30 - 175), (0, 0, 0), currentdict["opD"])
+
+A_rect = A_text.get_rect(center = opA.button.center)
+B_rect = B_text.get_rect(center = opB.button.center)
+C_rect = C_text.get_rect(center = opC.button.center)
+D_rect = D_text.get_rect(center = opD.button.center)
 
 def panel():
     global width, height
+    global currentdict
+    global currentanswer
     running = True
-    opC = options(500, 175, 125, (height - 30 - 175), (0, 0, 0), currentdict["opC"])    #op is short for option
-    opD = options(500, 175, (width - 500 - 125), (height - 30 - 175), (0, 0, 0), currentdict["opD"])
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -47,9 +85,24 @@ def panel():
                 running = False
                 sys.exit()
 
+        if opA.draw():
+            currentanswer = currentdict["opA"]
+        if opB.draw():
+            currentanswer = currentdict["opB"]
+        if opC.draw():
+            currentanswer = currentdict["opC"]
+        if opD.draw():
+            currentanswer = currentdict["opD"]
+
         window.fill((255,255,255))
+        opA.draw()
+        opB.draw()
         opC.draw()
         opD.draw()
+        window.blit(A_text, A_rect)
+        window.blit(B_text, B_rect)
+        window.blit(C_text, C_rect)
+        window.blit(D_text, D_rect)
         pygame.display.flip()
 
 def answcheck():
